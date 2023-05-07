78531e9bf81aaf4c51e54d379ecee65fbd053c57
Vediniz <veruskalima138@gmail.com>
2023-05-07 11:56:12 -0300

    updated project with missing models and configurations
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

--- backend/website/website/urls.py
+++ backend/website/website/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("bulbdat/", include("bulbdat.urls")),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin_site"),
 ]
