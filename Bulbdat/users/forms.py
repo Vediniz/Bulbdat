ebfb5d7d490df5903447cc5b7993f58f4b6297e0
Vediniz <veruskalima138@gmail.com>
2023-05-25 19:24:09 -0300

    :(
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

--- Bulbdat/users/forms.py
+++
@@ -1,7 +0,0 @@
-#write a django login form
-
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
